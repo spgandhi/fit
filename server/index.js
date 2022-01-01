fe0252686346135324dd4bdae550d5eae98a98e6
Shreyans <spgandhi@live.com>
2022-01-01 13:05:26 +0000

    add ssl config to apollo

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
-const { gql, ApolloServer } = require("apollo-server");
+const { gql } = require("apollo-server");
 const { Neo4jGraphQL } = require("@neo4j/graphql");
+const ApolloServer = require('apollo-server-express');
 const neo4j = require("neo4j-driver");
 require("dotenv").config();
+const express = require('express')
+const fs = require('fs');
+const https = require('https');
+const http = require('http');
 
 const typeDefs = gql`
   type User {
@@ -35,19 +40,62 @@ const typeDefs = gql`
   }   
 `;
 
+async function startApolloServer() {
+  const configurations = {
+    // Note: You may need sudo to run on port 443
+    production: { ssl: true, port: 4000, hostname: 'localhost' },
+    development: { ssl: false, port: 4000, hostname: 'fitbud.me' },
+  };
 
-const driver = neo4j.driver(
-  process.env.NEO4J_URI,
-  neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
-);
+  console.log(process.env.NODE_ENV);
+  const environment = process.env.NODE_ENV || 'development';
+  const config = configurations[environment];
 
-const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
+  const driver = neo4j.driver(
+    process.env.NEO4J_URI,
+    neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
+  );
 
-const server = new ApolloServer({
-  schema: neoSchema.schema,
-  context: ({ req }) => ({ req }),
-});
+  const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
+  const server = new ApolloServer.ApolloServer({
+    schema: neoSchema.schema,
+    context: ({ req }) => ({ req }),
+  });
 
-server.listen().then(({ url }) => {
-  console.log(`GraphQL server ready on ${url}`);
-});
+  await server.start();
+
+  const app = express();
+  server.applyMiddleware({ app });
+
+  // Create the HTTPS or HTTP server, per configuration
+  let httpServer;
+  console.log(config.ssl);
+  if (config.ssl) {
+    // Assumes certificates are in a .ssl folder off of the package root.
+    // Make sure these files are secured.
+    httpServer = https.createServer(
+      {
+        key: fs.readFileSync(`./etc/letsencrypt/live/fitbud.me/privkey.pem`),
+        cert: fs.readFileSync(`./etc/letsencrypt/live/fitbud.me/cert.pem`),
+      },
+      app,
+    );
+  } else {
+    httpServer = http.createServer(app);
+  }
+
+  await new Promise(resolve =>
+    httpServer.listen({ port: config.port }, resolve)
+  );
+
+  console.log(
+    '🚀 Server ready at',
+    `http${config.ssl ? 's' : ''}://${config.hostname}:${config.port}${
+      server.graphqlPath
+    }`
+  );
+
+  return { server, app };
+}
+
+startApolloServer();
